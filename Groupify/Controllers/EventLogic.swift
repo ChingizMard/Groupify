87ca0f562f88f1d198e7eaa406fc37c1fc3e5bce
Sandesh Bhandari <sandesh_bhandari007@hotmail.com>
2018-04-15 03:01:14 -0400

    Some random pod files

--- Groupify/Controllers/EventLogic.swift
+++ Groupify/Controllers/EventLogic.swift
@@ -9,30 +9,30 @@
 import Foundation
 
 
-class Event{
+class EventLogic{
     var _url = "http://localhost:4000/activity/find_activity"
     
-    static func getEvents(_ completion: @escaping (_ success: Bool, _ result: [Contact]?, _ error: String?) -> Void) {
-        let url = APIService.buildAuthURL(servicePath)
-        
-        APIService.get(url).responseJSON {
-            response in
-            switch response.result {
-            case .failure(_):
-                let errorString = JSON(data: response.data!)["error"].string
-                completion(false, nil, errorString)
-                return
-            case .success:
-                // Parse the JSON response
-                let objects = Mapper<Contact>().mapArray(JSONObject: response.result.value!)
-                
-                RealmUtils.store(objects)
-                
-                completion(true, objects, nil)
-                return
-                
-            }
-        }
-    }
+//    static func getEvents(_ completion: @escaping (_ success: Bool, _ result: [Contact]?, _ error: String?) -> Void) {
+//        let url = APIService.buildAuthURL(servicePath)
+//
+//        APIService.get(url).responseJSON {
+//            response in
+//            switch response.result {
+//            case .failure(_):
+//                let errorString = JSON(data: response.data!)["error"].string
+//                completion(false, nil, errorString)
+//                return
+//            case .success:
+//                // Parse the JSON response
+//                let objects = Mapper<Contact>().mapArray(JSONObject: response.result.value!)
+//
+//                RealmUtils.store(objects)
+//
+//                completion(true, objects, nil)
+//                return
+//
+//            }
+//        }
+//    }
     
 }
